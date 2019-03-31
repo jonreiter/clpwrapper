3caa39123b83bde2ac6bd170e34cfa5d4e0db3c8
jonreiter@gmail.com <jonreiter@gmail.com>
2019-03-31 22:03:18 +0800

    migrate initial code into repo

---
+++ wrappers.go
@@ -0,0 +1,54 @@
+// Package clpwrapper contains simple wrappers to help with lanl/clp
+// once go has an approximately-standard sparse matrix
+// package this code can go into the clp itself
+package clpwrapper
+
+import (
+	"github.com/james-bowman/sparse"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/floats"
+	"github.com/lanl/clp"
+)
+
+// equality tolerance
+const tolerance = 1e-5
+
+// GoNumMatrixToCLPPackedMatrix converts a likely-sparse mat.Matrix into a CoinPackedMatrix
+func GoNumMatrixToCLPPackedMatrix(matrix mat.Matrix) *clp.PackedMatrix {
+	nRows, nCols := matrix.Dims()
+	packedMat := clp.NewPackedMatrix()
+    packedMat.Reserve(nCols, nRows * nCols, false)
+        
+    for c:=0;c<nCols;c++ {
+        col := make([]clp.Nonzero, 0)
+        for r:=0;r<nRows;r++ {
+            thisVal := matrix.At(r,c)
+            if !floats.EqualWithinAbs(thisVal, 0.0, tolerance) {
+                col = append(col, clp.Nonzero{Index: r, Value: thisVal,})
+            }
+        }
+        packedMat.AppendColumn(col)
+    }
+	return packedMat
+}
+
+// CSCToCLPPackedMatrix converts a sparse.CSC into a CoinPackedMatrix
+func CSCToCLPPackedMatrix(matrix *sparse.CSC) *clp.PackedMatrix {
+	_, nCols := matrix.Dims()
+	totalNNZ := matrix.NNZ()
+	packedMat := clp.NewPackedMatrix()
+    packedMat.Reserve(nCols, totalNNZ, false)
+	
+    for c:=0;c<nCols;c++ {
+		col := make([]clp.Nonzero, 0)
+		matrix.DoColNonZero(c,func(i, j int, v float64) {
+              ele := clp.Nonzero{Index: i, Value: v, }
+			col = append(col, ele)
+		})
+        packedMat.AppendColumn(col)
+    }
+    
+	return packedMat
+}
+
+// eof
